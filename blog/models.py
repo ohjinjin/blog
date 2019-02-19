73465468cb7f15f8d41b762750198bd0d87323fc
ohjinjin <ohjinjin0408@naver.com>
2019-02-19 19:46:31 +0900

    the Create & Read functions

--- blog/models.py
+++ blog/models.py
@@ -9,3 +9,6 @@ class Blog(models.Model):
     # 제목 변경 래퍼함수
     def __str__(self):
         return self.title
+
+    def summary(self):
+        return self.body[:100]
--- secondeproject/urls.py
+++ secondeproject/urls.py
@@ -20,4 +20,8 @@ import blog.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blog.views.home, name="home"),
+    path('blog/<int:blog_id>',blog.views.detail,name="detail"),
+    path('blog/new/', blog.views.new, name='new'),
+    path('blog/create/', blog.views.create, name='create'),
+    path('blog/allList/',blog.views.allList, name='allList'),
 ]
